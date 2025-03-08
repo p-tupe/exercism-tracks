c88d0cc41cddd238676e5e8c0018743f7bf2ed6f
Pritesh Tupe <mail@priteshtupe.com>
2025-03-07 20:05:52 -0500

    Add go exercises

---
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,29 @@
+package purchase
+
+import "strings"
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	filler := " is clearly the better choice."
+	if strings.Compare(option1, option2) < 0 {
+		return option1 + filler
+	} else {
+		return option2 + filler
+	}
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age < 10 {
+		return originalPrice * 0.7
+	} else {
+		return originalPrice * 0.5
+	}
+}
